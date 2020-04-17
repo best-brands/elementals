72307302b0def40f8e098a4e0ce9de0d7ef88369
Harm Smits <info@poppedweb.com>
2020-04-17 19:26:07 +0200

    Update

--- babel.config.js
+++ babel.config.js
@@ -6,7 +6,7 @@ module.exports = function (api) {
             "@babel/preset-env",
             {
                 "useBuiltIns": "usage",
-                "corejs": 3,
+                "corejs": '3.6.5',
             }
         ]
     ];
