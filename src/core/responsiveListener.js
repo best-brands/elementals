3b6fb670e90c02951d923a9e7b29c504d2a550f3
Harm Smits <info@poppedweb.com>
2020-05-03 13:59:10 +0200

    Remove useless function wrapper

--- src/core/responsiveListener.js
+++ src/core/responsiveListener.js
@@ -3,9 +3,7 @@ import {default as debounce} from './debounce';
 import {addEventListener} from "./DOMEvents";
 
 export default function () {
-    (function () {
-        addEventListener(window, "resize", debounce(function () {
-            ResponsiveController.calculateViewport();
-        }, 200, false))
-    })();
+    addEventListener(window, "resize", debounce(function () {
+        ResponsiveController.calculateViewport();
+    }, 200, false))
 }
